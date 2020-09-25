2f143da2f96a056f9fe4cd03b6625fbd59740f3c
KonstantinBiaCo <kbc.off@list.ru>
2020-09-25 08:24:15 +0300

    TODO

---
+++ study-code-space/homework-3/sorter.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream> 
+#include <cstdlib>
+using namespace std;
+char alphabet
+int main()
+{
+    char buff[32];
+    ifstream fin("input.txt");
+    fin.getline(buff, 32);
+
+    return 0;
+}
