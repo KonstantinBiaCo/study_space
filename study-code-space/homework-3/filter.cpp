029f66d7f36e6e473bffe31879f85add7a988ebb
Konstantin V.S <51021474+KonstantinBiaCo@users.noreply.github.com>
2020-09-25 12:28:51 +0300

    Update filter.cpp

--- study-code-space/homework-3/filter.cpp
+++ study-code-space/homework-3/filter.cpp
@@ -5,23 +5,26 @@ using namespace std;
 int main()
 {
     
-    char nums[11] = {'0','1','2','3','4','5','6','7','8','9',' '};
+    char nums[11] = {'0','1','2','3','4','5','6','7','8','9'};
     //SetConsoleCP(1251);
     //SetConsoleOutputCP(1251);
     char new_string[255];
     char buff[255];
+    int pointer = 0;
     ifstream fin("input.txt");
 	while(true)
 	{
 		if (fin.eof()) break;
 		fin.getline(buff, 255);
+                pointer = 0;
         for(int i = 0; i < 255; i++)
         {
-            for(int j = 0; j < 11; j++)
+            for(int j = 0; j < 10; j++)
             {
                 if (buff[i] == nums[j]) 
                 {
-                    new_string[i] = nums[j];
+                    new_string[pointer] = nums[j];
+                    pointer++;
                 }
             }
             if (buff[i] == '\n') continue;
